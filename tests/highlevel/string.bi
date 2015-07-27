9bc707075672c997c449cca8505ab09c7ab87a3f
dkl <daniel.c.klauer@web.de>
2015-07-27 15:47:55 +0200

    tests: Add test case for -string on byte array

--- tests/highlevel/string.bi
+++ tests/highlevel/string.bi
@@ -8,5 +8,6 @@ extern s2(0 to 9) as myubyte
 declare sub f1(byval p1 as zstring ptr, byval p2 as ubyte ptr)
 declare sub f2(byval p3 as byte ptr, byval p4 as zstring ptr)
 declare sub f3(byval p5 as myubyte ptr)
+extern s3(0 to 9) as zstring
 
 end extern
--- tests/highlevel/string.h
+++ tests/highlevel/string.h
@@ -1,4 +1,4 @@
-// @fbfrog -string s1 -string p1 -string p4 -string p5
+// @fbfrog -string s1 -string p1 -string p4 -string p5 -string s3
 
 typedef unsigned char myubyte;
 extern myubyte s1[10];
@@ -7,3 +7,5 @@ extern myubyte s2[10];
 void f1(signed char *p1, unsigned char *p2);
 void f2(signed char *p3, unsigned char *p4);
 void f3(myubyte *p5);
+
+extern signed char s3[10];
