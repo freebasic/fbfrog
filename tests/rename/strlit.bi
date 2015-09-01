a7cbaaf4e2caff4e99b874c27ca59b1b96509d44
dkl <daniel.c.klauer@web.de>
2015-09-01 21:10:25 +0200

    bug: -rename[_] options affect string/char literals

---
+++ tests/rename/strlit.bi
@@ -0,0 +1,4 @@
+#pragma once
+
+#define M1 "a_"
+#define M2 asc("b_")
---
+++ tests/rename/strlit.h
@@ -0,0 +1,6 @@
+// @fbfrog -rename a a_ -rename_ b
+
+// -rename[_] options shouldn't affect string/char literals
+
+#define M1 "a"
+#define M2 'b'
