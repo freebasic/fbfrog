3b25532e150a6bd2fb0250e94e93bd3494bea1de
Daniel Klauer <daniel.c.klauer@web.de>
2017-10-30 07:58:43 +0100

    Add cpp test cases where macro call args contain newlines

---
+++ tests/cpp/expand/stringify-space.h
@@ -0,0 +1,25 @@
+#define m(a) #a
+
+static char a[] = m(
+ a b
+);
+
+static char a[] = m(
+);
+
+static char a[] = m( a);
+
+static char a[] = m(
+a);
+
+static char a[] = m(a
+b);
+
+static char a[] = m(
+a
+b);
+
+static char a[] = m(
+a
+b
+);
