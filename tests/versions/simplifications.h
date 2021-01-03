freebasic/fbfrog

commit f0ec0ded7e8d2f04120c95bfbb7b785b30f267b8

Turn AstNode into a "class"

src/ast-merge.bas
@@ -161,10 +161,10 @@ private sub hWrapStructFieldsInVerblocks(byval api as ApiBits, byval code as Ast
 		i = i->nxt
 	wend
 
-	if astIsMergableBlock(code) then
-		var newfields = astNewVERBLOCK(api, astCloneChildren(code))
-		astRemoveChildren(code)
-		astAppend(code, newfields)
+	if code->isMergableBlock() then
+		var newfields = astNewVERBLOCK(api, code->cloneChildren())
+		code->removeChildren()
+		code->append(newfields)
 	end if
 end sub
 
@@ -185,16 +185,16 @@ private sub hVerblockAppend _
 	var verblock = n->tail
 	if verblock andalso astIsVERBLOCK(verblock) then
 		if verblock->apis.equals(apis) then
-			astAppend(verblock, child)
+			verblock->append(child)
 			exit sub
 		end if
 	end if
 
-	astAppend(n, astNewVERBLOCK(apis, child))
+	n->append(astNewVERBLOCK(apis, child))
 end sub
 
 private sub hAddDecl(byval c as AstNode ptr, byval array as DECLNODE ptr, byval i as integer)
-	hVerblockAppend(c, array[i].apis, astClone(array[i].n))
+	hVerblockAppend(c, array[i].apis, array[i].n->clone())
 end sub
 
 ''
@@ -326,7 +326,7 @@ private sub hAddMergedDecl _
 	'' even if they have different children)
 	''
 	dim mdecl as AstNode ptr
-	if astIsMergableBlock(adecl) then
+	if adecl->isMergableBlock() then
 
 		''
 		'' For example:
@@ -359,22 +359,22 @@ private sub hAddMergedDecl _
 		''             field c as integer
 		''
 
-		var achildren = astCloneChildren(adecl)
-		var bchildren = astCloneChildren(bdecl)
+		var achildren = adecl->cloneChildren()
+		var bchildren = bdecl->cloneChildren()
 
 		'' Merge both set of children
 		var mchildren = astMergeVerblocks(achildren, bchildren)
 
 		'' Create a result block with the new set of children
-		mdecl = astCloneNode(adecl)
-		astAppend(mdecl, mchildren)
+		mdecl = adecl->cloneNode()
+		mdecl->append(mchildren)
 	else
 		'' "Merge" a and b by cloning a. They've compared equal in astIsEqual() so this works.
 		'' Below we only need to cover a few additional cases where astIsEqual() is more permissive
 		'' than a true equality check: it allows merging of a/b even if they're slightly different.
 		'' This currently affects the calling convention only. In such cases, just cloning a isn't
 		'' enough and some actual merging work is needed.
-		mdecl = astClone(adecl)
+		mdecl = adecl->clone()
 
 		hFindCommonCallConvsOnMergedDecl(mdecl, adecl, bdecl)
 	end if
@@ -647,8 +647,8 @@ function astMergeVerblocks _
 	hAstMerge(c, atable.array, 0, atable.count - 1, _
 	             btable.array, 0, btable.count - 1)
 
-	astDelete(a)
-	astDelete(b)
+	delete a
+	delete b
 
 	function = c
 end function
@@ -699,7 +699,7 @@ private sub hSolveOutRedundantVerblocks(byval code as AstNode ptr, byval parenta
 			'' Nested verblock covers at least the parent's versions?
 			if i->apis.coversAtLeast(parentapis) then
 				'' Remove this verblock, preserve only its children
-				astReplace(code, i, astCloneChildren(i))
+				code->replace(i, i->cloneChildren())
 			end if
 		else
 			hSolveOutRedundantVerblocks(i, parentapis)
@@ -709,7 +709,7 @@ private sub hSolveOutRedundantVerblocks(byval code as AstNode ptr, byval parenta
 	wend
 end sub
 
-#define hCalcIfConditionWeight(expr) astCount(expr)
+#define hCalcIfConditionWeight(expr) ((expr)->count())
 
 '' If the #if block's condition expression is bigger than that of
 '' the #else block, swap the two blocks.
@@ -818,7 +818,7 @@ private sub hTurnVerblocksIntoPpIfs(byval code as AstNode ptr)
 			end if
 
 			'' Insert #endif
-			astInsert(code, astNew(ASTKIND_PPENDIF), j)
+			code->insert(astNew(ASTKIND_PPENDIF), j)
 		end if
 
 		i = i->nxt
@@ -847,7 +847,7 @@ end destructor
 
 function CondCounter.find(byval cond as AstNode ptr) as integer
 	for i as integer = 0 to condcount - 1
-		if astIsEqual(conds[i].cond, cond) then
+		if astIsEqual(conds[i].cond, cond, FALSE) then
 			return i
 		end if
 	next
@@ -965,14 +965,14 @@ private sub replaceUnixChecks(byval veror as AstNode ptr)
 		if astIsDEFINED(i) then
 			for os as integer = 0 to OS__COUNT - 1
 				if osinfo(os).is_unix andalso (*i->text = *osinfo(os).fbdefine) then
-					astRemove(veror, i)
+					veror->remove(i)
 					exit for
 				end if
 			next
 		end if
 		i = nxt
 	wend
-	astAppend(veror, astNewDEFINED("__FB_UNIX__"))
+	veror->append(astNewDEFINED("__FB_UNIX__"))
 end sub
 
 private function hSimplify(byval n as AstNode ptr, byref changed as integer) as AstNode ptr
@@ -982,7 +982,7 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 	scope
 		var i = n->head
 		while i
-			var newi = hSimplify(astClone(i), changed)
+			var newi = hSimplify(i->clone(), changed)
 
 			'' Don't add a VEROR as a child of a VEROR; instead, add
 			'' the VEROR's chidlren to the VEROR's parent. (same for VERANDs)
@@ -990,15 +990,15 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 				newi->kind = ASTKIND_GROUP
 			end if
 
-			i = astReplace(n, i, newi)
+			i = n->replace(i, newi)
 		wend
 	end scope
 
 	'' Single child, or none at all? Solve out the VEROR/VERAND.
 	if n->head = n->tail then
 		changed = TRUE
-		function = astClone(n->head)
-		astDelete(n)
+		function = n->head->clone()
+		delete n
 		exit function
 	end if
 
@@ -1007,9 +1007,9 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 	end if
 
 	'' Solve out "complete" VERORs - VERORs that cover all possible choices
-	if astGroupContains(frog.completeverors, n) then
+	if frog.completeverors->groupContains(n) then
 		changed = TRUE
-		astDelete(n)
+		delete n
 		return NULL
 	end if
 
@@ -1038,7 +1038,7 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 	if mostcommoncond = NULL then
 		return n
 	end if
-	mostcommoncond = astClone(mostcommoncond)
+	mostcommoncond = mostcommoncond->clone()
 
 	var extracted = astNew(ASTKIND_VEROR)
 
@@ -1048,9 +1048,9 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 		while verand
 			var verandnext = verand->nxt
 			if astIsVERAND(verand) then
-				if astGroupContains(verand, mostcommoncond) then
-					astAppend(extracted, astClone(verand))
-					astRemove(n, verand)
+				if verand->groupContains(mostcommoncond) then
+					extracted->append(verand->clone())
+					n->remove(verand)
 				end if
 			end if
 			verand = verandnext
@@ -1067,8 +1067,8 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 			var cond = verand->head
 			while cond
 				var condnext = cond->nxt
-				if astIsEqual(cond, mostcommoncond) then
-					astRemove(verand, cond)
+				if astIsEqual(cond, mostcommoncond, FALSE) then
+					verand->remove(cond)
 				end if
 				cond = condnext
 			wend
@@ -1076,7 +1076,7 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 			'' If this VERAND now only contains 1 condition,
 			'' solve out the VERAND
 			if verand->head = verand->tail then
-				astReplace(extracted, verand, astClone(verand->head))
+				extracted->replace(verand, verand->head->clone())
 			end if
 
 			verand = verandnext
@@ -1090,7 +1090,7 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 	assert(astIsVERAND(extracted))
 
 	'' And re-add that to the original VEROR
-	astAppend(n, extracted)
+	n->append(extracted)
 	changed = TRUE
 
 	function = n
@@ -1113,7 +1113,7 @@ private sub hMaybeEmitRangeCheck(byval veror as AstNode ptr, byref l as integer,
 	if (coveredcount <= 2) and l_in_mid and r_in_mid then
 		'' A range check isn't worth it, re-add the normal individual checks
 		for i as integer = l to r
-			astAppend(veror, astNewVERNUMCHECK(i))
+			veror->append(astNewVERNUMCHECK(i))
 		next
 		exit sub
 	end if
@@ -1136,7 +1136,7 @@ private sub hMaybeEmitRangeCheck(byval veror as AstNode ptr, byref l as integer,
 		exit sub
 	end if
 
-	astAppend(veror, check)
+	veror->append(check)
 end sub
 
 ''
@@ -1192,7 +1192,7 @@ private sub hFoldNumberChecks(byval n as AstNode ptr)
 
 			if i->kind = ASTKIND_VERNUMCHECK then
 				covered(i->vernum) = TRUE
-				astRemove(n, i)
+				n->remove(i)
 			end if
 
 			i = nxt
@@ -1234,7 +1234,7 @@ private function foldUnix(byval n as AstNode ptr) as AstNode ptr
 	scope
 		var i = n->head
 		while i
-			i = astReplace(n, i, foldUnix(astClone(i)))
+			i = n->replace(i, foldUnix(i->clone()))
 		wend
 	end scope
 
@@ -1248,7 +1248,7 @@ private function foldUnix(byval n as AstNode ptr) as AstNode ptr
 	'' Single child, or none at all? Solve out the VEROR/VERAND.
 	if n->head = n->tail then
 		var child = n->head
-		astUnlink(n, child)
+		n->unlink(child)
 		n = child
 	end if
 
@@ -1284,17 +1284,17 @@ private function hBuildIfConditionFor(byval bits as ApiBits) as AstNode ptr
 	for i as integer = 0 to frog.apicount - 1
 		if bits.isSet(i) then
 			var api = @frog.apis[i]
-			var verand = astClone(api->verand)
+			var verand = api->verand->clone()
 
 			'' Conditions for API's target
 			if osinfo(api->target.os).has_arm then
-				astPrepend(verand, astNewDEFINEDfbarm(not archinfo(api->target.arch).is_arm))
+				verand->prepend(astNewDEFINEDfbarm(not archinfo(api->target.arch).is_arm))
 			end if
 			if osinfo(api->target.os).has_64bit then
-				astPrepend(verand, astNewDEFINEDfb64(not archinfo(api->target.arch).is_64bit))
+				verand->prepend(astNewDEFINEDfb64(not archinfo(api->target.arch).is_64bit))
 			end if
 			if frog.enabledoscount > 1 then
-				astPrepend(verand, astNewDEFINEDfbos(api->target.os))
+				verand->prepend(astNewDEFINEDfbos(api->target.os))
 			end if
 
 			var veror = astNewVEROR(verand)
@@ -1330,7 +1330,7 @@ private sub hGenVerExprs(byval code as AstNode ptr)
 			'' the check was always true -- thus, we can remove this
 			'' #if check and insert the body in its place.
 			if i->expr = NULL then
-				astReplace(code, i, astCloneChildren(i))
+				code->replace(i, i->cloneChildren())
 			end if
 		end if
 
@@ -1341,7 +1341,7 @@ end sub
 #define astIsPPIFWithVERAND(n) (astIsPPIF(n) andalso astIsVERAND(n->expr))
 #define astIsPPELSEIFWithVERAND(n) (astIsPPELSEIF(n) andalso astIsVERAND(n->expr))
 #define astIsPPELSEWithVERAND(n) (astIsPPELSE(n) andalso astIsVERAND(n->expr))
-#define hasVerandPrefix(n, prefix) astIsEqual(n->expr->head, prefix)
+#define hasVerandPrefix(n, prefix) astIsEqual(n->expr->head, prefix, FALSE)
 
 private function findEndifOfVerandIfBlockWithCommonPrefix(byval n as AstNode ptr) as AstNode ptr
 	if astIsPPIFWithVERAND(n) = FALSE then exit function
@@ -1384,22 +1384,22 @@ private sub splitVerandIfsIntoNestedIfs(byval ast as AstNode ptr)
 		if iendif then
 			'' New outer #if with just the prefix condition
 			var newif = astNew(ASTKIND_PPIF)
-			newif->expr = astClone(i->expr->head)
+			newif->expr = i->expr->head->clone()
 
 			'' Move all the old #if/#elseifs/#else/#endif into the new #if
 			inext = iendif->nxt
-			astTakeAndAppendChildSequence(newif, ast, i, iendif)
+			newif->takeAndAppendChildSequence(ast, i, iendif)
 
 			'' Insert the new #if in the old one's place
-			astInsert(ast, newif, inext)
-			astInsert(ast, astNew(ASTKIND_PPENDIF), inext)
+			ast->insert(newif, inext)
+			ast->insert(astNew(ASTKIND_PPENDIF), inext)
 
 			'' Cut prefix from the VERANDs of the nested #if/#elseifs/#else
 			scope
 				var nested = newif->head
 				do
 					var verand = nested->expr
-					astRemove(verand, verand->head)
+					verand->remove(verand->head)
 
 					'' Solve out the VERAND if only 1 expression left
 					assert(verand->head)
@@ -1407,7 +1407,7 @@ private sub splitVerandIfsIntoNestedIfs(byval ast as AstNode ptr)
 						nested->expr = verand->head
 						verand->head = NULL
 						verand->tail = NULL
-						astDelete(verand)
+						delete verand
 					end if
 
 					nested = nested->nxt
@@ -1444,12 +1444,12 @@ private sub mergeSiblingIfs(byval ast as AstNode ptr)
 				var secondif = iendif->nxt
 				if secondif andalso astIsPPIF(secondif) then
 					'' Same condition?
-					if astIsEqual(i->expr, secondif->expr) then
+					if astIsEqual(i->expr, secondif->expr, FALSE) then
 						'' Merge 1st into 2nd
 						'' (we know the 1st doesn't have any #elseifs/#else, but the 2nd might)
-						astTakeAndPrependChildren(secondif, i)
-						astRemove(ast, i)
-						astRemove(ast, iendif)
+						secondif->takeAndPrependChildren(i)
+						ast->remove(i)
+						ast->remove(iendif)
 						nxt = secondif
 					end if
 				end if
@@ -1493,8 +1493,8 @@ private sub removeUnnecessaryIfNesting(byval ast as AstNode ptr)
 						nested->expr = NULL
 						nested->head = NULL
 						nested->tail = NULL
-						astDelete(nested)
-						astDelete(nestedendif)
+						delete nested
+						delete nestedendif
 					end if
 				end if
 			end if
@@ -1564,8 +1564,8 @@ end sub
 ''    A    and    B         maybe, but not necessarily.
 ''    A    and    not A     are definitely opposite.
 private function conditionsAreOpposite(byval a as AstNode ptr, byval b as AstNode ptr) as integer
-	if astIsNOT(a) and (not astIsNOT(b)) then return astIsEqual(a->head, b)
-	if astIsNOT(b) and (not astIsNOT(a)) then return astIsEqual(b->head, a)
+	if astIsNOT(a) and (not astIsNOT(b)) then return astIsEqual(a->head, b, FALSE)
+	if astIsNOT(b) and (not astIsNOT(a)) then return astIsEqual(b->head, a, FALSE)
 	function = FALSE
 end function
 

src/ast.bi
@@ -247,8 +247,9 @@ type AstVisitor extends object
 	declare abstract function visit(byref as AstNode) as integer
 end type
 
-'' When changing, adjust astClone(), astIsEqual(), astDump*()
-'' TODO: pack
+type ASTVISITCALLBACK as function(byval as AstNode ptr) as integer
+
+'' When changing, adjust clone(), astIsEqual(), dump*()
 type AstNode_
 	kind		as integer  '' ASTKIND_*
 	attrib		as integer  '' ASTATTRIB_*
@@ -286,12 +287,55 @@ type AstNode_
 	'' Linked list of child nodes, operands/fields/parameters/...
 	as AstNode ptr head, tail, nxt, prev
 
+	declare function getIndexOf(byval lookfor as AstNode ptr) as integer
+	declare function hasChild(byval lookfor as AstNode ptr) as integer
+	declare sub takeChildren(byval s as AstNode ptr)
+	declare sub takeAndPrependChildren(byval s as AstNode ptr)
+	declare sub takeAndAppendChildSequence(byval s as AstNode ptr, byval first as AstNode ptr, byval last as AstNode ptr)
+	declare function cloneChildren() as AstNode ptr
+	declare function groupContains(byval lookfor as AstNode ptr) as integer
+	declare function groupContainsAnyChildrenOf(byval other as AstNode ptr) as integer
+	declare function groupContainsAllChildrenOf(byval other as AstNode ptr) as integer
+	declare function groupsContainEqualChildren(byval other as AstNode ptr) as integer
+	declare destructor()
+	declare sub insert(byval n as AstNode ptr, byval ref as AstNode ptr)
+	declare sub prepend(byval n as AstNode ptr)
+	declare sub append(byval n as AstNode ptr)
+	declare sub unlink(byval n as AstNode ptr)
+	declare function remove(byval n as AstNode ptr) as AstNode ptr
+	declare sub removeChildren()
+	declare function replace(byval old as AstNode ptr, byval n as AstNode ptr) as AstNode ptr
+	declare sub setText(byval newtext as zstring ptr)
+	declare sub setAlias(byval newalias as zstring ptr)
+	declare sub renameSymbol(byval newid as zstring ptr)
+	declare sub renameSymbolWithoutSettingOrigId(byval newid as zstring ptr)
+	declare sub takeAliasFromId(byval src as AstNode ptr)
+	declare sub takeOrigId(byval src as AstNode ptr)
+	declare sub takeAliasAndOrigId(byval src as AstNode ptr)
+	declare sub copyOrigId(byval src as AstNode ptr)
+	declare sub setType(byval dtype as integer, byval subtype as AstNode ptr)
+	declare function cloneNode() as AstNode ptr
+	declare function clone() as AstNode ptr
+	declare function contains(byval astkind as integer) as integer
+	declare function containsCAssignments() as integer
+	declare function has1Child() as integer
+	declare function hasOnlyChild(byval astkind as integer) as integer
+	declare function isCodeBlock() as integer
+	declare function isCodeScopeBlock() as integer
+	declare function isScopeBlockWith1Stmt() as integer
+	declare function isMergableBlock() as integer
+	declare function isCastTo(byval dtype as integer, byval subtype as AstNode ptr) as integer
+	declare function evalConstiAsInt64() as longint
+	declare function isConst0() as integer
+	declare function lookupMacroParam(byval id as zstring ptr) as integer
+	declare function getMacroParamByNameIgnoreCase(byval id as zstring ptr) as AstNode ptr
 	declare sub visit(byref visitor as AstVisitor)
+	declare sub visit(byval callback as ASTVISITCALLBACK)
+	declare function count() as integer
+	declare function dumpPrettyDecl(byval show_type as integer) as string
+	declare function dumpOne() as string
+	declare sub dump(byval nestlevel as integer = 0, byref prefix as string = "")
 end type
-
-'' result = boolean = whether to visit this node's children
-'' (can be used to skip #define bodies, etc.)
-type ASTVISITCALLBACK as function(byval as AstNode ptr) as integer
 
 #define astNewTEXT(text) astNew(ASTKIND_TEXT, text)
 #define astNewDEFINED(id) astNew(ASTKIND_DEFINED, id)
@@ -324,70 +368,13 @@ declare function astNewDEFINEDfb64(byval negate as integer) as AstNode ptr
 declare function astNewDEFINEDfbarm(byval negate as integer) as AstNode ptr
 declare function astNewDEFINEDfbos(byval os as integer) as AstNode ptr
 declare function astNewOPTION(byval opt as integer, byval text1 as zstring ptr = NULL, byval text2 as zstring ptr = NULL) as AstNode ptr
-declare sub astTakeChildren(byval d as AstNode ptr, byval s as AstNode ptr)
-declare sub astTakeAndPrependChildren(byval d as AstNode ptr, byval s as AstNode ptr)
-declare sub astTakeAndAppendChildSequence(byval d as AstNode ptr, byval s as AstNode ptr, byval first as AstNode ptr, byval last as AstNode ptr)
-declare function astCloneChildren(byval src as AstNode ptr) as AstNode ptr
-declare function astGroupContains(byval group as AstNode ptr, byval lookfor as AstNode ptr) as integer
-declare function astGroupContainsAnyChildrenOf(byval l as AstNode ptr, byval r as AstNode ptr) as integer
-declare function astGroupContainsAllChildrenOf(byval l as AstNode ptr, byval r as AstNode ptr) as integer
-declare sub astDelete(byval n as AstNode ptr)
-declare sub astInsert(byval parent as AstNode ptr, byval n as AstNode ptr, byval ref as AstNode ptr)
-declare sub astPrepend(byval parent as AstNode ptr, byval n as AstNode ptr)
-declare sub astAppend(byval parent as AstNode ptr, byval n as AstNode ptr)
-declare sub astUnlink(byval parent as AstNode ptr, byval n as AstNode ptr)
-declare function astRemove(byval parent as AstNode ptr, byval a as AstNode ptr) as AstNode ptr
-declare sub astRemoveChildren(byval parent as AstNode ptr)
-declare function astReplace _
-	( _
-		byval parent as AstNode ptr, _
-		byval old as AstNode ptr, _
-		byval n as AstNode ptr _
-	) as AstNode ptr
-declare sub astSetText(byval n as AstNode ptr, byval text as zstring ptr)
-declare sub astSetAlias(byval n as AstNode ptr, byval alias_ as zstring ptr)
-declare sub astRenameSymbol(byval n as AstNode ptr, byval newid as zstring ptr)
-declare sub astRenameSymbolWithoutSettingOrigId(byval n as AstNode ptr, byval newid as zstring ptr)
-declare sub astTakeAliasFromId(byval dst as AstNode ptr, byval src as AstNode ptr)
-declare sub astTakeOrigId(byval dst as AstNode ptr, byval src as AstNode ptr)
-declare sub astTakeAliasAndOrigId(byval dst as AstNode ptr, byval src as AstNode ptr)
-declare sub astCopyOrigId(byval dst as AstNode ptr, byval src as AstNode ptr)
-declare sub astSetType _
-	( _
-		byval n as AstNode ptr, _
-		byval dtype as integer, _
-		byval subtype as AstNode ptr _
-	)
-declare function astCloneNode(byval n as AstNode ptr) as AstNode ptr
-declare function astClone(byval n as AstNode ptr) as AstNode ptr
-declare function astContains(byval n as AstNode ptr, byval astkind as integer) as integer
-declare function astContainsCAssignments(byval n as AstNode ptr) as integer
-declare function astHas1Child(byval n as AstNode ptr) as integer
-declare function astHasOnlyChild(byval n as AstNode ptr, byval astkind as integer) as integer
-declare function astIsCodeBlock(byval n as AstNode ptr) as integer
-declare function astIsCodeScopeBlock(byval n as AstNode ptr) as integer
-declare function astIsScopeBlockWith1Stmt(byval n as AstNode ptr) as integer
-declare function astIsMergableBlock(byval n as AstNode ptr) as integer
-declare function astIsCastTo(byval n as AstNode ptr, byval dtype as integer, byval subtype as AstNode ptr) as integer
+
 declare function astIsEqual _
 	( _
 		byval a as AstNode ptr, _
 		byval b as AstNode ptr, _
-		byval is_merge as integer = FALSE _
+		byval is_merge as integer _
 	) as integer
-declare function hGetFbNumberLiteralPrefix(byval attrib as integer) as string
-declare function astEvalConstiAsInt64(byval n as AstNode ptr) as longint
-declare function astIsConst0(byval n as AstNode ptr) as integer
-declare function astLookupMacroParam(byval macro as AstNode ptr, byval id as zstring ptr) as integer
-declare function astGetMacroParamByNameIgnoreCase(byval macro as AstNode ptr, byval id as zstring ptr) as AstNode ptr
-declare sub astVisit(byval n as AstNode ptr, byval callback as ASTVISITCALLBACK)
-declare function astCount(byval n as AstNode ptr) as integer
+
 declare function astDumpPrettyKind(byval astkind as integer) as string
-declare function astDumpPrettyDecl(byval n as AstNode ptr, byval show_type as integer = FALSE) as string
-declare function astDumpOne(byval n as AstNode ptr) as string
-declare sub astDump _
-	( _
-		byval n as AstNode ptr, _
-		byval nestlevel as integer = 0, _
-		byref prefix as string = "" _
-	)
+declare function hGetFbNumberLiteralPrefix(byval attrib as integer) as string

src/c-common.bas
@@ -79,7 +79,7 @@ constructor CNumLitParser _
 end constructor
 
 destructor CNumLitParser()
-	astDelete(ast)
+	delete ast
 end destructor
 
 function CNumLitParser.takeAst() as AstNode ptr
@@ -508,7 +508,7 @@ private sub hMacroParam(byref tk as TokenBuffer, byref x as integer, byval macro
 
 	var param = astNew(ASTKIND_MACROPARAM, id)
 	param->attrib or= maybevariadic
-	astAppend(macro, param)
+	macro->append(param)
 	macro->attrib or= maybevariadic
 	macro->paramcount += 1
 end sub

commit aed66ab78f5aa35dad88d16fe390aeda7424c9e3

Fix crash regression
AstNode.clone() could be called on a NULL THIS pointer and then crashed.

This is a regression from commit f0ec0ded, which replaced astClone(n)
and astCloneNode(n), which had the NULL checks, by clone() and
cloneNode() methods in the AstNode "class", which don't have the NULL
check anymore.

src/ast-merge.bas
@@ -997,9 +997,12 @@ private function hSimplify(byval n as AstNode ptr, byref changed as integer) as 
 	'' Single child, or none at all? Solve out the VEROR/VERAND.
 	if n->head = n->tail then
 		changed = TRUE
-		function = n->head->clone()
+		dim child as AstNode ptr = NULL
+		if n->head then
+			child = n->head->clone()
+		end if
 		delete n
-		exit function
+		return child
 	end if
 
 	if astIsVEROR(n) = FALSE then

tests/versions/simplifications.bi
@@ -1,0 +1,13 @@
+#pragma once
+
+#if V = 1
+	#define exists_only_in_V1
+#else
+	#define exists_only_in_V2
+#endif
+
+#define exists_in_all_versions
+
+#if (defined(__FB_64BIT__) and defined(__FB_UNIX__)) or ((not defined(__FB_64BIT__)) and (defined(__FB_WIN32__) or defined(__FB_UNIX__))) or defined(__FB_DOS__)
+	#define exists_not_for_win64
+#endif

tests/versions/simplifications.fbfrog
@@ -1,0 +1,7 @@
+-declareversions V 1 2
+-selectversion
+-case 1
+	-define V 1
+-case 2
+	-define V 2
+-endselect

tests/versions/simplifications.h
@@ -1,0 +1,17 @@
+// @fbfrog <dir>simplifications.fbfrog
+
+#if V == 1
+	// An #if block should be generated for these, since their presence depends on the value of V.
+	#define exists_only_in_V1
+#else
+	#define exists_only_in_V2
+#endif
+
+// No #if block should be generated for this, since it's present in all APIs
+#define exists_in_all_versions
+
+#ifndef _WIN64
+	// For this, an #if block should be generated, but the condition should not check V,
+	// since this is present for all V values.
+	#define exists_not_for_win64
+#endif